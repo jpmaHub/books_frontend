a2614ccfdcebb6aee97c6fa61d8645cea4869f75
Jaseera Abubacker <j.p.mohamedabubacker@gmail.com>
2018-08-24 10:23:39 +0100

    Add title and header

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './header.css';
+
+export default function Title() {
+  return (
+    <header className="App-header">
+      <h1 className="App-title">Welcome to React </h1>
+    </header>
+  );
+}
+
---
+++ src/components/Title/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './title.css';
+
+export default function Title() {
+  return (
+    <h4 className="App-intro">
+      Search Books by name
+    </h4>
+  );
+}
+
